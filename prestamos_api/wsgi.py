fe0008354dc23a788874fbd828c3cd857fef27a4
Jesus Axel Gonzalez Basilio <2312jagb@gmail.com>
2025-07-06 00:47:36 -0600

    Corrijo estilos en el login

--- prestamos_api/wsgi.py
+++ prestamos_api/wsgi.py
@@ -14,3 +14,19 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prestamos_api.settings')
 
 application = get_wsgi_application()
+
+
+# prestamos_api/wsgi.py
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prestamos_api.settings')
+
+application = get_wsgi_application()
+
+# Llamar a creación del superusuario si aplica
+try:
+    from .create_superuser import create_admin
+    create_admin()
+except Exception as e:
+    print(f"Error creando superusuario: {e}")
